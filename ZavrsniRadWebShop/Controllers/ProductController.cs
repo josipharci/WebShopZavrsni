0fcd6b644932037dabe64d28e42b0798763687f4
Josip Harci <81259271+josipharci@users.noreply.github.com>
2024-01-11 14:06:22 +0100

    Update

---
+++ ZavrsniRadWebShop/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZavrsniRadWebShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
