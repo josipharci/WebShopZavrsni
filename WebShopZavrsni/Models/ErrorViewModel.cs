f82da7b85d3cd10fe95fd6ea736fca657c7ff7b1
Josip Harci <81259271+josipharci@users.noreply.github.com>
2024-01-11 12:55:31 +0100

    Update

--- WebShopZavrsni/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebShopZavrsni.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
