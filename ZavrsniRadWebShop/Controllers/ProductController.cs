3614171f503b9008291da0b985c404beb0026e57
Josip Harci <81259271+josipharci@users.noreply.github.com>
2024-01-29 20:05:29 +0100

    Update

--- ZavrsniRadWebShop/Controllers/ProductController.cs
+++ ZavrsniRadWebShop/Controllers/ProductController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZavrsniRadWebShop.Models;
 
 namespace ZavrsniRadWebShop.Controllers
 {
     public class ProductController : Controller
     {
+        public static List<Product> product = new List<Product>{
+              new Product { Name="Samesung" , Description="Najbolji mobitel na svijetu" , Price=15},
+              new Product { Name="Samesung sm" , Description="Najbolji mobitel na svijetu" , Price=16}
+        };
+  
+
         public IActionResult Index()
         {
+            return View(product);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        // POST: AdminProduct/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index"); 
+            }
+
+            return View(product); 
         }
     }
 }
---
+++ ZavrsniRadWebShop/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace ZavrsniRadWebShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; } = 0.0M;
+        public int CategoryId { get; set; }  
+        public string Description { get; set; }
+    
+        public virtual Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
